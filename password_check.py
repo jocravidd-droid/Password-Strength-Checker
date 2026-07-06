b5e1c9391937f162db47ef7bd5f1bfe40b399161
jocravidd-droid <jocravidd@gmail.com>
2026-07-06 16:16:27 +0200

    Mise a jour
    Je l'ai mis a jour recemment avec mais connaissance a jour !!

---
+++ password_check.py
@@ -0,0 +1,147 @@
+import time
+import os
+
+print(f"{' ANALYSE PASSWORD ':-^35}\n")
+
+user = input("User : ")
+
+def check_length(password):
+
+    if len(str(password)) > 8:
+        return True
+
+def check_uppercase(password):
+
+    for caractére in password:
+
+        if caractére.isupper():
+            return True
+        else:
+            continue
+    
+def check_digit(password):
+
+    for caractére in password:
+
+        if caractére.isdigit():
+            return True
+        else:
+            continue
+
+def check_special_char(password):
+    
+    speciaux = "!@#$%^&*"
+
+    for caractére in speciaux:
+
+        if caractére in speciaux:
+            return True
+        else:
+            continue
+
+def evaluate_password(password):
+
+    counter = 0
+
+    if check_length(password) == True:
+        counter += 1
+
+    if check_uppercase(password) == True:
+        counter += 1
+
+    if check_digit(password) == True:
+        counter += 1
+
+    if check_special_char(password) == True:
+        counter += 1
+
+    if counter == 1:
+        print(f"{user} your password is Faible\n")
+
+    if counter == 2:
+        print(f"{user} your password is Faible\n")
+
+    if counter == 3:
+        print(f"{user} your password is Moyen\n")
+    
+    if counter == 4:
+        print(f"{user} your password is Fort\n")
+
+def evaluate_password_premium(password):
+
+    counter = 0
+
+    print("\nVerification en cours...\n")
+    time.sleep(1)
+    
+    if check_length(password) == True:
+        counter += 1
+
+    if check_uppercase(password) == True:
+        counter += 1
+
+    if check_digit(password) == True:
+        counter += 1
+
+    if check_special_char(password) == True:
+        counter += 1
+    
+    courente_password = ["123456", "password", "azerty"]
+
+    if password in courente_password:
+        counter -= 1
+
+    if counter == 1:
+        for i in range(0,11):
+            c = int(i)
+            print(f"\rChargement du resultat : [{'█' * c}]\n", end="", flush=True)
+            time.sleep(0.9)
+            os.system('clear')
+            
+        print(f"{user} your password is Faible")
+
+    if counter == 2:
+        for i in range(0,11):
+            c = int(i)
+            print(f"\rChargement du resultat : [{'█' * c}]\n", end="", flush=True)
+            time.sleep(0.9)
+            os.system('clear')
+
+        print(f"{user} your password is Faible")
+
+    if counter == 3:
+        for i in range(0,11):
+            c = int(i)
+            print(f"\rChargement du resultat : [{'█' * c}]\n", end="", flush=True)
+            time.sleep(0.9)
+            os.system('clear')
+
+        print(f"{user} your password is Moyen")
+    
+    if counter == 4:
+        for i in range(0,11):
+            c = int(i)
+            print(f"\rChargement du resultat : [{'█' * c}]\n", end="", flush=True)
+            time.sleep(0.9)
+            os.system('clear')
+
+        print(f"\n{user} your password is Fort")
+
+    print("Fin de L'analyse !\n")
+
+systeme_actif = True
+
+while systeme_actif:
+    
+    choix = input("Entrez un mot de passe (ou 'quitter') : ")
+
+    if choix == "quitter":
+        systeme_actif = False
+        print(f"{' Arrêt du système '.upper():-^35}")
+    elif choix == 'Mode secu +':
+        print("Lancement de l'analyse premium")
+        password = input('Password : ')
+        evaluate_password_premium(password)
+    else:
+        print("\nAnalyse en cours...\n")
+        evaluate_password(choix)
